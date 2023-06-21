e359c2b03e9ee40c481c0df0247a14b6893a91fa
orcungurer <orcungurer@hotmail.com>
2023-06-21 13:31:54 +0300

    added dummy cocktail data

---
+++ src/components/data.ts
@@ -0,0 +1,51 @@
+export const DUMMY_COCKTAILS = [
+  {
+    id: 1,
+    name: "Jagerbomb",
+    category: "Cocktail",
+    image:
+      "https://www.thespruceeats.com/thmb/HEcHMz3CP-zjQSgdvlEEfYBqO6o=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/jager-bomb-recipe-759713-hero-01-54560094d6cd4d97bbea810aa07e772b.jpg",
+    ingredients: [
+      {
+        name: "Jagermeister",
+        amount: "1.5 oz",
+      },
+      {
+        name: "Energy drink",
+        amount: "4 oz",
+      },
+    ],
+    instructions: [
+      "Fill a shot glass with Jagermeister.",
+      "Pour an energy drink into a glass.",
+      "Drop the shot glass into the glass and drink.",
+    ],
+    garnish: "None",
+    notes:
+      "It is recommended to drink this cocktail quickly to enjoy the combined flavors.",
+  },
+  {
+    id: 2,
+    name: "Jager & Beer",
+    category: "Cocktail",
+    image:
+      "https://d1ynl4hb5mx7r8.cloudfront.net/wp-content/uploads/2019/06/12163841/b0b056327034f03aaeda.png",
+    ingredients: [
+      {
+        name: "Jagermeister",
+        amount: "1.5 oz",
+      },
+      {
+        name: "Beer",
+        amount: "12 oz",
+      },
+    ],
+    instructions: [
+      "Pour a shot glass amount of Jagermeister into a glass.",
+      "Fill the rest of the glass with beer.",
+      "Stir gently to mix the ingredients.",
+    ],
+    garnish: "None",
+    notes: "You can use your favorite type of beer for this cocktail.",
+  },
+];
