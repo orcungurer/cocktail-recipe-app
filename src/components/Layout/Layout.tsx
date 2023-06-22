41ef13a7fa86674af9a186fac147a15070025904
orcungurer <orcungurer@hotmail.com>
2023-06-22 13:25:22 +0300

    created MainNavigation and rendered on Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,10 @@
 import classes from "./Layout.module.css";
+import MainNavigation from "./MainNavigation";
 
 const Layout: React.FC<{ children: React.ReactNode }> = (props) => {
   return (
     <div>
+      <MainNavigation />
       <main className={classes.main}>{props.children}</main>
     </div>
   );
---
+++ src/components/Layout/MainNavigation.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import classes from "./MainNavigation.module.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <div className={`${classes.logo} ${inter.className}`}>
+        Cocktail Recipe
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/" className={inter.className}>
+              All Cocktails
+            </Link>
+          </li>
+          <li>
+            <Link href="/new-cocktail" className={inter.className}>
+              Suggest New Cocktail
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
