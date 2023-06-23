51b6f2fd13009745851e7def2d4d86780872589f
orcungurer <orcungurer@hotmail.com>
2023-06-23 15:06:57 +0300

    prerendered cocktails on Home with getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import { Fragment } from "react";
 import CocktailList from "@/components/cocktails/CocktailList";
 import { DUMMY_COCKTAILS } from "@/components/data";
+import { CocktailsProps } from "@/models/cocktails";
 
-const HomePage = () => {
+const HomePage: React.FC<CocktailsProps> = (props) => {
   return (
     <Fragment>
-      <CocktailList cocktails={DUMMY_COCKTAILS} />
+      <CocktailList cocktails={props.cocktails} />
     </Fragment>
   );
 };
 
 export default HomePage;
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      cocktails: DUMMY_COCKTAILS,
+    },
+  };
+};
