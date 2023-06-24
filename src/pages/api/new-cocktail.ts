896f5a45a4f9add4977387bbd4365fb22678e307
orcungurer <orcungurer@hotmail.com>
2023-06-25 00:16:40 +0300

    provided mongodb connection for api route

--- src/pages/api/new-cocktail.ts
+++ src/pages/api/new-cocktail.ts
@@ -1,13 +1,33 @@
 // POST /api/new-cocktail
 import { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 type ResponseData = {
   message: string;
 };
 
-function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
   if (req.method === "POST") {
     const data = req.body;
+
+    // console.log("process.env", process.env);
+
+    const mongodbUri: string = process.env.MONGODB_URI ?? "";
+    
+    const client = await MongoClient.connect(mongodbUri);
+
+    const db = client.db();
+
+    const suggestedCocktailsCollection = db.collection("new-cocktail");
+
+    const result = await suggestedCocktailsCollection.insertOne(data);
+
+    console.log("result", result);
+    
+    client.close();
 
     res.status(201).json({ message: "Sent cocktail data successfully!" });
   }
