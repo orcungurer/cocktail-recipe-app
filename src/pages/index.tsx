2ae32ab16dad0e2d3b692f6c8ca8096fa9af7ec4
orcungurer <orcungurer@hotmail.com>
2023-06-26 22:46:35 +0300

    getting data from mongodb instead of dummy data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { Fragment } from "react";
 import CocktailList from "@/components/cocktails/CocktailList";
-import { DUMMY_COCKTAILS } from "@/components/data";
+// import { DUMMY_COCKTAILS } from "@/components/data";
 import { CocktailsProps } from "@/models/cocktails";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const HomePage: React.FC<CocktailsProps> = (props) => {
+  console.log(props.cocktails);
+  
   return (
     <Fragment>
       <CocktailList cocktails={props.cocktails} />
@@ -14,9 +20,29 @@ const HomePage: React.FC<CocktailsProps> = (props) => {
 export default HomePage;
 
 export const getStaticProps = async () => {
+  const mongodbUri = process.env.MONGODB_URI ?? "";
+
+  const client = await MongoClient.connect(mongodbUri);
+
+  const db = client.db();
+
+  const cocktailsCollection = db.collection("cocktails");
+
+  const cocktails = await cocktailsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      cocktails: DUMMY_COCKTAILS,
+      cocktails: cocktails.map((cocktail) => ({
+        id: cocktail._id.toString(),
+        cocktailId: cocktail.cocktailId,
+        name: cocktail.name,
+        image: cocktail.image,
+        ingredients: cocktail.ingredients,
+        instructions: cocktail.instructions,
+        notes: cocktail.notes,
+      })),
     },
     revalidate: 1,
   };
