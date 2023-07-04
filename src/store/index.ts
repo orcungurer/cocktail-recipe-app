25333138241cf7516390bd30a4629c21e4ed8d33
orcungurer <orcungurer@hotmail.com>
2023-07-04 18:42:24 +0300

    created redux store

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import filterSlice from "./filter-slice";
+
+const store = configureStore({
+  reducer: {
+    filter: filterSlice.reducer,
+  },
+});
+
+export default store;
+
+// useSelector's state
+// usage: const .. = useSelector((state: RootState) => ..);
+export type RootState = ReturnType<typeof store.getState>
