e4b9f7464ce8242e48e6a08dd2d6ae4ebb1993f8
orcungurer <orcungurer@hotmail.com>
2023-07-05 19:34:34 +0300

    rendering filteredCocktails due to ingredients

--- src/components/cocktails/CocktailList.tsx
+++ src/components/cocktails/CocktailList.tsx
@@ -1,11 +1,35 @@
 import classes from "./CocktailList.module.css";
 import CocktailItem from "./CocktailItem";
 import { CocktailsProps } from "@/models/cocktails";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
 
 const CocktailList: React.FC<CocktailsProps> = (props) => {
+  const selectedIngredients = useSelector(
+    (state: RootState) => state.filter.selectedIngredients
+  );
+
+  console.log(selectedIngredients);
+
+  // we return a cocktail if no ingredients selected,
+  // or if the cocktail includes a selected ingredient.
+  const filteredCocktails = props.cocktails.filter((cocktail) => {
+    if (
+      selectedIngredients.length === 0 ||
+      cocktail.ingredients.some((ingredient) =>
+        selectedIngredients.includes(ingredient.name)
+      )
+    ) {
+      return true;
+    }
+    return false;
+  });
+
+  console.log(filteredCocktails);
+
   return (
     <ul className={classes.list}>
-      {props.cocktails.map((cocktail) => (
+      {filteredCocktails.map((cocktail) => (
         <CocktailItem
           key={cocktail.id}
           id={cocktail.id}
