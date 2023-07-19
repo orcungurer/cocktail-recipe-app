49f0cfb910e7c743ea7c4830927d4f3c01bd1fe2
orcungurer <orcungurer@hotmail.com>
2023-07-19 17:00:50 +0300

    outsourced select & list logic (home to cocktails)

---
+++ src/components/cocktails/Cocktails.tsx
@@ -0,0 +1,92 @@
+import classes from "./Cocktails.module.css";
+import { Fragment, useId } from "react";
+import CocktailList from "@/components/cocktails/CocktailList";
+import { CocktailsProps } from "@/models/cocktails";
+import Select, { ActionMeta, CSSObjectWithLabel } from "react-select";
+import { useDispatch, useSelector } from "react-redux";
+import { filterActions } from "@/store/filter-slice";
+import { RootState } from "@/store";
+
+interface IngredientOption {
+  value: string;
+  label: string;
+}
+
+const Cocktails: React.FC<CocktailsProps> = (props) => {
+  const dispatch = useDispatch();
+
+  const selectedIngredients = useSelector(
+    (state: RootState) => state.filter.selectedIngredients
+  );
+
+  const selectedIngredientsAsDefault = selectedIngredients.map(
+    (ingredient) => ({
+      value: ingredient,
+      label: ingredient,
+    })
+  );
+
+  // by flattenning cocktails, we map each cocktail's ingredients
+  // then we map through each ingredient and get the name
+  // and then we create a new set to remove duplications
+  // after that we convert the set back into an array
+  // thus we can sort it alphabetically since its array
+  // finally we map again to set value and label properties
+  const ingredientOptions: IngredientOption[] = Array.from(
+    new Set(
+      props.cocktails.flatMap((cocktail) =>
+        cocktail.ingredients.map((ingredient) => ingredient.name)
+      )
+    )
+  )
+    .sort((a, b) => a.localeCompare(b))
+    .map((name) => ({ value: name, label: name }));
+
+  const selectIngredientsHandler = (
+    newValue: readonly IngredientOption[],
+    actionMeta: ActionMeta<IngredientOption>
+  ) => {
+    const mutableValue = Array.from(newValue); // or [...newValue]
+
+    console.log("mutableValue", mutableValue);
+    console.log("actionMeta", actionMeta);
+
+    if (actionMeta.action === "select-option") {
+      dispatch(filterActions.addIngredient(actionMeta.option!.value));
+    } else if (actionMeta.action === "remove-value") {
+      dispatch(filterActions.removeIngredient(actionMeta.removedValue.value));
+    } else if (actionMeta.action === "clear") {
+      dispatch(filterActions.clearIngredient());
+    }
+  };
+
+  // to prevent provided error, used CSSObjectWithLabel as type
+  const customStyles = {
+    control: (provided: CSSObjectWithLabel) => ({
+      ...provided,
+      fontFamily: "Inter, Arial, sans-serif",
+    }),
+    option: (provided: CSSObjectWithLabel) => ({
+      ...provided,
+      fontFamily: "Inter, Arial, sans-serif",
+    }),
+  };
+
+  return (
+    <Fragment>
+      <Select
+        styles={customStyles}
+        placeholder={"Select Ingredients..."}
+        defaultValue={selectedIngredientsAsDefault}
+        instanceId={useId()}
+        isMulti
+        name="ingredients"
+        options={ingredientOptions}
+        onChange={selectIngredientsHandler}
+      />
+      <CocktailList cocktails={props.cocktails} />
+    </Fragment>
+  );
+};
+
+export default Cocktails;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,82 +1,15 @@
-import { Fragment, useId } from "react";
-import CocktailList from "@/components/cocktails/CocktailList";
+import { Fragment } from "react";
 // import { DUMMY_COCKTAILS } from "@/components/data";
 import { CocktailsProps } from "@/models/cocktails";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import Head from "next/head";
-import Select, { ActionMeta, CSSObjectWithLabel } from "react-select";
-import { useDispatch, useSelector } from "react-redux";
-import { filterActions } from "@/store/filter-slice";
-import { RootState } from "@/store";
+import Cocktails from "@/components/cocktails/Cocktails";
 
 dotenv.config();
 
-interface IngredientOption {
-  value: string;
-  label: string;
-}
-
 const HomePage: React.FC<CocktailsProps> = (props) => {
   console.log(props.cocktails);
-  const dispatch = useDispatch();
-
-  const selectedIngredients = useSelector(
-    (state: RootState) => state.filter.selectedIngredients
-  );
-
-  const selectedIngredientsAsDefault = selectedIngredients.map(
-    (ingredient) => ({
-      value: ingredient,
-      label: ingredient,
-    })
-  );
-
-  // by flattenning cocktails, we map each cocktail's ingredients
-  // then we map through each ingredient and get the name
-  // and then we create a new set to remove duplications
-  // after that we convert the set back into an array
-  // thus we can sort it alphabetically since its array
-  // finally we map again to set value and label properties
-  const ingredientOptions: IngredientOption[] = Array.from(
-    new Set(
-      props.cocktails.flatMap((cocktail) =>
-        cocktail.ingredients.map((ingredient) => ingredient.name)
-      )
-    )
-  )
-    .sort((a, b) => a.localeCompare(b))
-    .map((name) => ({ value: name, label: name }));
-
-  const selectIngredientsHandler = (
-    newValue: readonly IngredientOption[],
-    actionMeta: ActionMeta<IngredientOption>
-  ) => {
-    const mutableValue = Array.from(newValue); // or [...newValue]
-
-    console.log("mutableValue", mutableValue);
-    console.log("actionMeta", actionMeta);
-
-    if (actionMeta.action === "select-option") {
-      dispatch(filterActions.addIngredient(actionMeta.option!.value));
-    } else if (actionMeta.action === "remove-value") {
-      dispatch(filterActions.removeIngredient(actionMeta.removedValue.value));
-    } else if (actionMeta.action === "clear") {
-      dispatch(filterActions.clearIngredient());
-    }
-  };
-
-  // to prevent provided error, used CSSObjectWithLabel as type
-  const customStyles = {
-    control: (provided: CSSObjectWithLabel) => ({
-      ...provided,
-      fontFamily: "Inter, Arial, sans-serif",
-    }),
-    option: (provided: CSSObjectWithLabel) => ({
-      ...provided,
-      fontFamily: "Inter, Arial, sans-serif",
-    }),
-  };
 
   return (
     <Fragment>
@@ -87,17 +20,7 @@ const HomePage: React.FC<CocktailsProps> = (props) => {
           content="Explore our extensive collection of cocktails and find your perfect drink! Easily search for specific cocktails or utilize our ingredient filter feature to discover cocktails you can create using the ingredients you already have."
         />
       </Head>
-      <Select
-        styles={customStyles}
-        placeholder={"Select Ingredients..."}
-        defaultValue={selectedIngredientsAsDefault}
-        instanceId={useId()}
-        isMulti
-        name="ingredients"
-        options={ingredientOptions}
-        onChange={selectIngredientsHandler}
-      />
-      <CocktailList cocktails={props.cocktails} />
+      <Cocktails cocktails={props.cocktails} />
     </Fragment>
   );
 };
