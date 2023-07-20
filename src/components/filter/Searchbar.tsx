06dbc5d4f283c83c7fe4c415fee2f651e330909e
orcungurer <orcungurer@hotmail.com>
2023-07-20 16:57:49 +0300

    added Searchbar component and its css

---
+++ src/components/filter/Searchbar.tsx
@@ -0,0 +1,31 @@
+import classes from "./Searchbar.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { filterActions } from "@/store/filter-slice";
+import { RootState } from "@/store";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"]});
+
+const Searchbar = () => {
+  const dispatch = useDispatch();
+  const searchbar = useSelector((state: RootState) => state.filter.searchbar);
+
+  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(filterActions.setSearchbar(event.target.value));
+  };
+
+  return (
+    <div className={classes.searchbar}>
+      <input
+        type="text"
+        id="search"
+        value={searchbar}
+        className={inter.className}
+        onChange={inputChangeHandler}
+        placeholder="Search Cocktail"
+      />
+    </div>
+  );
+};
+
+export default Searchbar;
