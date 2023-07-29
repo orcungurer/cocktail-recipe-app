0ffd9c2b1a224a332cf2fc3ceba0af6d215c40af
orcungurer <orcungurer@hotmail.com>
2023-07-29 20:02:11 +0300

    added font & placeholder for inputs. updated -> ?

--- src/components/cocktails/NewCocktailForm.tsx
+++ src/components/cocktails/NewCocktailForm.tsx
@@ -8,9 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 interface NewCocktailFormProps {
   onAddCocktail: (cocktailData: {
     name: string;
-    image?: string;
-    ingredients?: string;
-    instructions?: string;
+    // image?: string;
+    ingredients: string;
+    instructions: string;
     notes?: string;
     date: string;
   }) => void;
@@ -18,7 +18,7 @@ interface NewCocktailFormProps {
 
 const NewCocktailForm: React.FC<NewCocktailFormProps> = (props) => {
   const name = useRef<HTMLInputElement>(null);
-  const image = useRef<HTMLInputElement>(null);
+  // const image = useRef<HTMLInputElement>(null);
   const ingredients = useRef<HTMLInputElement>(null);
   const instructions = useRef<HTMLInputElement>(null);
   const notes = useRef<HTMLInputElement>(null);
@@ -28,9 +28,9 @@ const NewCocktailForm: React.FC<NewCocktailFormProps> = (props) => {
     event.preventDefault();
 
     const enteredName = name.current!.value;
-    const enteredImage = image.current?.value;
-    const enteredIngredients = ingredients.current?.value;
-    const enteredInstructions = instructions.current?.value;
+    // const enteredImage = image.current?.value;
+    const enteredIngredients = ingredients.current!.value;
+    const enteredInstructions = instructions.current!.value;
     const enteredNotes = notes.current?.value;
 
     if (enteredName.trim().length === 0) {
@@ -39,7 +39,7 @@ const NewCocktailForm: React.FC<NewCocktailFormProps> = (props) => {
 
     const cocktailData = {
       name: enteredName,
-      image: enteredImage,
+      // image: enteredImage,
       ingredients: enteredIngredients,
       instructions: enteredInstructions,
       notes: enteredNotes,
@@ -49,6 +49,13 @@ const NewCocktailForm: React.FC<NewCocktailFormProps> = (props) => {
     props.onAddCocktail(cocktailData);
   };
 
+  const placeholders = {
+    name: "Jager & Beer",
+    ingredients: "Jagermeister (1.5oz), Beer (12oz)",
+    instructions: "Step 1, Step 2, Step 3, ..",
+    notes: "It is recommended that ...",
+  };
+
   return (
     <Card className={classes.card}>
       <form onSubmit={submitHandler} className={classes.form}>
@@ -56,31 +63,58 @@ const NewCocktailForm: React.FC<NewCocktailFormProps> = (props) => {
           <label htmlFor="name" className={inter.className}>
             Cocktail Name
           </label>
-          <input type="text" id="name" ref={name} required />
+          <input
+            type="text"
+            id="name"
+            ref={name}
+            required
+            className={inter.className}
+            placeholder={placeholders.name}
+          />
         </div>
-        <div className={classes.control}>
+        {/* <div className={classes.control}>
           <label htmlFor="image" className={inter.className}>
             Cocktail Image
           </label>
           <input type="text" id="image" ref={image} />
-        </div>
+        </div> */}
         <div className={classes.control}>
           <label htmlFor="ingredients" className={inter.className}>
             Cocktail Ingredients
           </label>
-          <input type="text" id="ingredients" ref={ingredients} />
+          <input
+            type="text"
+            id="ingredients"
+            ref={ingredients}
+            required
+            className={inter.className}
+            placeholder={placeholders.ingredients}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="intructions" className={inter.className}>
             Cocktail Instructions
           </label>
-          <input type="text" id="intructions" ref={instructions} />
+          <input
+            type="text"
+            id="intructions"
+            ref={instructions}
+            required
+            className={inter.className}
+            placeholder={placeholders.instructions}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="notes" className={inter.className}>
             Cocktail Notes
           </label>
-          <input type="text" id="notes" ref={notes} />
+          <input
+            type="text"
+            id="notes"
+            ref={notes}
+            className={inter.className}
+            placeholder={placeholders.notes}
+          />
         </div>
         <div className={classes.actions}>
           <button>Submit</button>
